91ed5f19bb41145013f7294900c7cf203ee6a0a7
YevheniiSkrypkin <81817746+YevheniiSkrypkin@users.noreply.github.com>
2021-07-29 17:23:38 +0300

    Add files via upload

--- ДЗ-31. DOM, BOM/js/solution.js
+++ ДЗ-31. DOM, BOM/js/solution.js
@@ -7,20 +7,9 @@ for (let forText of li) {
     console.log(forText);    
 }
 console.log(li.length)
-// let mainText = document.body.childNodes[1].textContent
-
 
 let a = document.body.firstElementChild.textContent
 
 console.log(Array.of(a))
 
-// let arr = Array.from(ul.childNodes);
-// console.log(arr)
-// console.log(arr.length)
-
-// console.log(ul.textContent)
-
-// let a = ul.textContent
-// let arr2 = Array.of(document.body.childNodes[1].childNodes[1].textContent)
-// console.log(arr2)
 
